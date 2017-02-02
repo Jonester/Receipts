57ee1858ed3287cc920cc867c65b99a7624b53da
Jonester <chris.jones78@gmail.com>
2017-02-02 11:34:16 -0500

    Initial Commit

---
+++ Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts
+//
+//  Created by Chris Jones on 2017-02-02.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
